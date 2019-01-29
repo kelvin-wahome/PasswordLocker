6f6edbe8a7c3e8017ec19a46c6b0cddfde4f7da0
blackrose <wahomekelving@gmail.com>
2019-01-29 15:16:55 +0300

    Created run.py and imported the classes

--- credentials.py
+++ credentials.py
@@ -2,3 +2,10 @@ class Credentials:
     """
     Class that generates new instances of credentials.
     """
+
+    credentials_list =[]
+
+    def __init__(self,account,name,password):
+        self.account_name = account
+        self.account_username = name
+        self.account_password = password
