970113bbfb423f41f6e8d9d573cd8dead6f7e168
blackrose <wahomekelving@gmail.com>
2019-01-29 10:53:55 +0300

    Created a user list  variable to store the users and instance variables

--- user.py
+++ user.py
@@ -2,3 +2,12 @@ class User:
     '''
     Class that generates new instances of contacts
     '''
+
+    user_list = []
+
+    def __init__(self,user_name,user_password):
+        '''
+        Method that helps us define user properties
+        '''
+        self.user_name = name
+        self.user_pasword = password
