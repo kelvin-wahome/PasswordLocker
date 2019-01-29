39ba863f823acc3b7e78002767faf9888338e191
blackrose <wahomekelving@gmail.com>
2019-01-29 16:40:30 +0300

    Added a provision for user to signup and save his account

--- run.py
+++ run.py
@@ -21,3 +21,23 @@ from credentials import Credentials
         Function to delete credentials
         '''
         credentials.delete_credentials()
+
+
+    def main():
+        print("PASSWORD LOCKER")
+        print("Hi welcome to password locker. What's your name?")
+            user_name = input()
+            print('\n')
+            while True:
+               print(f"Wassup {user_name}!!.Use these short codes: cu-create new user, cc-create new credentials,")
+
+               short_code = input().lower()
+               if short_code == 'cu':
+                   print("Sign Up")
+
+                   print("User name ...")
+                   u_name = input()
+                   print("Password ...")
+                   u_password = input()
+
+                   save_user(create_user(u_name,u_password)) #create and save user
