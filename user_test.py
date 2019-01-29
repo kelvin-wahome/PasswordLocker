059d39f6da22a63deb9638d54ca71cc2fb9af7a1
blackrose <wahomekelving@gmail.com>
2019-01-29 11:04:08 +0300

    Created a test class for the user class behaviours

--- user_test.py
+++ user_test.py
@@ -1,4 +1,7 @@
 import unittest # Importing the unittest module
 from user import User # import user class
 
-class TestUser(unittest.TestCase)
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the user class behaviours
+    '''
