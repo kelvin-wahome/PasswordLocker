1b2eb0b6eb513d032c924d0f4897ddeb9b53fbf3
blackrose <wahomekelving@gmail.com>
2019-01-29 16:07:27 +0300

    Added a function to delete credentials

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3.6
 from user  import User
 from credentials import Credentials
+
+    def create_user(name,password):
+        '''
+        Function to create a new user
+        '''
+        new_user = User(name,password)
+        return new_user
+
+
+    def save_user(user):
+        '''
+        Function to save user
+        '''
+        user.save_user()
+
+    def del_credentials(credentials):
+        '''
+        Function to delete credentials
+        '''
+        credentials.delete_credentials()
